e86dae4109bdc6ced28affb38cedadf052bb146c
Jonathan Edwards <Jonathan.Edwards@navihealth.com>
2019-02-14 15:30:10 -0600

    Created interfaces for track and album

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
-import {ProductService} from '../product.service'
+import { ProductService } from '../product.service';
+import { Album } from 'app/album';
 
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+    selector: 'app-product-description',
+    templateUrl: './product-description.component.html',
+    styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
+    albumInfo: Album;
 
-    albumInfo;
+    constructor(private _productService: ProductService) {}
 
-  constructor(private _productService: ProductService) { }
-
-  ngOnInit() {
-      this._productService.getAlbum(1).subscribe(r => this.albumInfo = r);
-  }
-
+    ngOnInit() {
+        this._productService.getAlbum(1).subscribe(r => (this.albumInfo = r));
+    }
 }
